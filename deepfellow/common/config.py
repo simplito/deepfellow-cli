f8069707c6dc62184f9b68bddd8e3f18bd094388
Piotr Zalewa <zalun@gmail.com>
2025-07-28 16:41:33 +0200

    refactor: no need to pass the context

--- deepfellow/common/config.py
+++ deepfellow/common/config.py
@@ -5,10 +5,11 @@ from copy import deepcopy
 from pathlib import Path
 from typing import Any
 
+import click
 import typer
 
 
-def get_config_path(ctx: typer.Context) -> Path:
+def get_config_path() -> Path:
     """Get config path from context.
 
     Args:
@@ -20,6 +21,7 @@ def get_config_path(ctx: typer.Context) -> Path:
     Raises:
        typer.BadParameter: If no config provided.
     """
+    ctx = click.get_current_context()
     if ctx.obj and ctx.obj.get("config-path"):
         config_path = ctx.obj["config-path"]
         # Ensure we return a Path object
@@ -31,11 +33,10 @@ def get_config_path(ctx: typer.Context) -> Path:
     raise typer.BadParameter("No config provided.")
 
 
-def load_config(ctx: typer.Context, raise_on_error: bool = False) -> dict[str, Any]:
+def load_config(raise_on_error: bool = False) -> dict[str, Any]:
     """Load and parse config file.
 
     Args:
-        ctx (typer.Context): CLI context object.
         raise_on_error (bool, optional): Whether to raise on error loading config. Defaults to False.
 
     Returns:
@@ -45,7 +46,7 @@ def load_config(ctx: typer.Context, raise_on_error: bool = False) -> dict[str, A
         FileNotFoundError: If no config file found
         Exit: If error loading config
     """
-    config_path = get_config_path(ctx)
+    config_path = get_config_path()
 
     try:
         content = config_path.read_text(encoding="utf-8")
@@ -70,18 +71,17 @@ def load_config(ctx: typer.Context, raise_on_error: bool = False) -> dict[str, A
     return {}
 
 
-def store_config(ctx: typer.Context, config_data_source: dict[str, Any], update: bool = True) -> None:
+def store_config(config_data_source: dict[str, Any], update: bool = True) -> None:
     """Store/save config data to file.
 
     Args:
-        ctx (typer.Context): Typer context
         config_data_source (dict[str, Any]): Config data to store
         update (bool): If True, load and update existing config
 
     Raises:
        Exit: If the config file cannot be written to disk and any OS error
     """
-    config_path = get_config_path(ctx)
+    config_path = get_config_path()
     config_data = deepcopy(config_data_source)
 
     if update:
