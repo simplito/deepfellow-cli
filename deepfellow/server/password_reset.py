8cccfa6aa0ec6cbb811e33cdea3d862e86394bc3
Piotr Zalewa <pzalewa@simplito.com>
2025-11-18 09:59:50 +0000

    Resolve "deepfellow server password-reset"

---
+++ deepfellow/server/password_reset.py
@@ -0,0 +1,21 @@
+"""Reset password command."""
+
+from pathlib import Path
+
+import typer
+
+from deepfellow.common.validation import validate_email
+from deepfellow.server.utils.options import directory_option
+from deepfellow.server.utils.users import reset_password as reset_password_util
+
+app = typer.Typer()
+
+
+@app.command()
+def password_reset(
+    directory: Path = directory_option("Target directory for the DeepFellow Server installation."),
+    email: str | None = typer.Argument(None, callback=validate_email, help="Email"),
+    password: str | None = typer.Argument(None, help="Password"),
+) -> None:
+    """Create admin."""
+    reset_password_util(directory, email, password)
